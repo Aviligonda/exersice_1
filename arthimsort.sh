0389585c943f1ce6f686f6169be08c463bf57bae
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-05-12 09:22:57 +0530

    sorting

--- arthimsort.sh
+++ arthimsort.sh
@@ -8,3 +8,17 @@ art[1]=$(($a*$b+$c));
 art[2]=$(($c+$a/$b));
 art[3]=$(($a%$b+$c));
 echo "before sorting :"${art[@]};
+
+for ((i=0;i<${#art[@]};i++))
+do
+    for ((j=1;j<${#art[@]};j++))
+     do
+           if [ ${art[$i]} -lt ${art[$j]} ]
+           then
+                  temp=${art[$i]};
+                  art[$i]=${art[$j]};
+                   art[$j]=$temp;
+            fi
+       done
+done
+echo "After sorting  an arry decending :"${art[@]};
