5a692d885609b8a4ed50a600d68d1441311805a8
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-05-12 08:55:47 +0530

    modified problem

--- arthimsort.sh
+++ arthimsort.sh
@@ -3,4 +3,8 @@ read -p "enter the value of a :" a;
 read -p "enter the value of b :" b;
 read -p "enter the value of c :" c;
 echo "$a $b $c";
-
+art[0]=$(($a+$b*$c));
+art[1]=$(($a*$b+$c));
+art[2]=$(($c+$a/$b));
+art[3]=$(($a%$b+$c));
+echo "before sorting :"${art[@]};
